664ff8a87a1cfa22dfc7b38cd5305819c966c410
Woomymy <woomy@woomy.be>
2025-06-07 15:36:16 +0200

    fix(events): Adapt embeds for new usernames system
    Change-Id: Ic5011167a15b89eb279afc6f884ce905135e3a8e

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -32,47 +32,50 @@ export default clientEvent({
             if (msg.channelId === channelId) continue;
 
             const channel = client.channels.cache.get(channelId) as TextChannel;
-            let webhook = (await channel.fetchWebhooks()).find(
-                (wh) => wh.name === INTERSERVER_WH_NAME
-            );
+            if (channel) {
 
-            if (!webhook) {
-                webhook = await channel.createWebhook({
-                    name: INTERSERVER_WH_NAME,
-                    reason: "Interserveur"
+                let webhook = (await channel.fetchWebhooks()).find(
+                    (wh) => wh.name === INTERSERVER_WH_NAME
+                );
+
+                if (!webhook) {
+                    webhook = await channel.createWebhook({
+                        name: INTERSERVER_WH_NAME,
+                        reason: "Interserveur"
+                    });
+                }
+
+                const lastMessage = (
+                    await msg.channel.messages.fetch({ limit: 2 })
+                ).last();
+
+                // Handle message replies
+                if (msg.type === MessageType.Reply) {
+                    const reference = await msg.fetchReference();
+                    if (!reference) return;
+
+                    msg.embeds.push(makeReplyEmbed(reference));
+                }
+
+                const whMessage = await webhook.send({
+                    content: `${
+                        lastMessage?.author.id === msg.author.id &&
+                        lastMessage?.guildId === msg.guildId
+                            ? ""
+                            : `***${
+                                  SERVERS_HEADERS[
+                                      msg.guildId as ServersHeadersKey
+                                  ] ?? `❓ ${msg.guild.name}`
+                              }***`
+                    }\n${msg.content}`,
+                    username: `${msg.author.displayName} (@${msg.author.username})`,
+                    avatarURL: msg.author.displayAvatarURL(),
+                    embeds: msg.embeds,
+                    allowedMentions: { parse: [] },
+                    files: msg.attachments.map((attachement) => attachement.url)
                 });
+                clones.push({ channelId, id: whMessage.id });
             }
-
-            const lastMessage = (
-                await msg.channel.messages.fetch({ limit: 2 })
-            ).last();
-
-            // Handle message replies
-            if (msg.type === MessageType.Reply) {
-                const reference = await msg.fetchReference();
-                if (!reference) return;
-
-                msg.embeds.push(makeReplyEmbed(reference));
-            }
-
-            const whMessage = await webhook.send({
-                content: `${
-                    lastMessage?.author.id === msg.author.id &&
-                    lastMessage?.guildId === msg.guildId
-                        ? ""
-                        : `***${
-                              SERVERS_HEADERS[
-                                  msg.guildId as ServersHeadersKey
-                              ] ?? `❓ ${msg.guild.name}`
-                          }***`
-                }\n${msg.content}`,
-                username: msg.author.username,
-                avatarURL: msg.author.displayAvatarURL(),
-                embeds: msg.embeds,
-                allowedMentions: { parse: [] },
-                files: msg.attachments.map((attachement) => attachement.url)
-            });
-            clones.push({ channelId, id: whMessage.id });
         }
 
         await client.prisma.message.create({
